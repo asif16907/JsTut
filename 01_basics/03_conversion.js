6ea76e5fdf4820edc60413bd6374f39f3c5a8161
asif16907 <58693073+asif16907@users.noreply.github.com>
2023-12-12 13:24:33 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,16 @@
+let score=null;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "something"==> NaN
+
+let loggedin=1
+let boolloogedin=Boolean(loggedin)
+
+console.log(boolloogedin);
+// "" ==> 0
+// "something" => 1
