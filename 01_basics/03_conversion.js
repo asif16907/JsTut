0f0fb789c529059aafa42e42da04f53933077ea7
asif16907 <58693073+asif16907@users.noreply.github.com>
2023-12-12 13:49:26 +0000

    comparison

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,16 +1,36 @@
 let score=null;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 // "something"==> NaN
 
 let loggedin=1
 let boolloogedin=Boolean(loggedin)
 
-console.log(boolloogedin);
+//console.log(boolloogedin);
 // "" ==> 0
 // "something" => 1
+
+
+
+// ***************** operations ******************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+"2");
+
+// type depend onthe first number/string
+
+
+
